3bb81e44abb25afc7df36d501b859ae4c5a79089
mymstudios <mymcreativehub@gmail.com>
2024-11-11 22:19:36 +0700

    skypack added

--- getBodies.js
+++ getBodies.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from 'https://cdn.skypack.dev/three';
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 const sceneMiddle = new THREE.Vector3(0, 0, 0);
