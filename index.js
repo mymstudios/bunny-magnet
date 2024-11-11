16a769b18d3ce534d4647f7cc172b101f40ea65b
mymstudios <mymcreativehub@gmail.com>
2024-11-11 22:00:44 +0700

    Trying to fix typo again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from '../node_modules/three/build/three.module.js';
 import { getBody, getMouseBall } from "./getBodies.js";
 import RAPIER from 'https://cdn.skypack.dev/@dimforge/rapier3d-compat@0.11.2';
-import { EffectComposer } from "../node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
+import { EffectComposer } from '../node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from "../node_modules/three/examples/jsm/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "../node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
 
